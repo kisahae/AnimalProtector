1797da2c0711e05f649ee499446c389ec4a2307b
Haenuki Sachi <sachihaenuki1@gmail.com>
2023-01-28 16:39:44 +0700

    First Upload

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,9 @@
+function expand() 
+{
+    var x = document.getElementById("ourcontent");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
